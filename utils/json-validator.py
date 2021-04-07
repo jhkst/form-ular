531dc35c4aaf82447e9c38dee5d58b138ec84224
jhkst <jhkst@centrum.cz>
2021-04-07 11:51:08 +0200

    Fixed empty local store bug, moved core script to separate file

--- utils/json-validator.py
+++ utils/json-validator.py
@@ -2,16 +2,21 @@
 
 import os
 import json
+import sys
 from jsonschema import validate, RefResolver
 
-with open("../../tmp/LeRa002854_DN_PODD_fixed.json") as instanceF:
+if len(sys.argv) < 3:
+    print("Syntax:\n   {} <json data file> <json schema file>".format(sys.argv[0]))
+    sys.exit(1);
+
+with open(sys.argv[1]) as instanceF:
     instance = json.load(instanceF)
-with open("jsd-schema.json") as schemaF:
+with open(sys.argv[2]) as schemaF:
     schema = json.load(schemaF)
 
 # this is a directory name (root) where the 'grandpa' is located
 dir = os.path.dirname(os.path.realpath(__file__))
 
 schema_path = 'file:///{0}/'.format(dir)
-resolver = RefResolver(schema_path, schema)
+resolver = RefResolver(schema_path, schema) # TODO: fix ref path
 validate(instance, schema, resolver=resolver)
