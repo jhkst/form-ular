22fea4db7613fe98a15071d87b02b9cacc3914b6
jhkst <jhkst@centrum.cz>
2021-04-24 21:31:05 +0200

    docker script imporved

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,1 +1,11 @@
-docker run -dit --name form-ular -p 8080:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4
+#!/bin/bash
+
+CONT_NAME="form-ular"
+PORT=8088
+
+if [ ! "$(docker ps -q  -f name=$CONT_NAME)]" ]; then
+  if [ "$(docker ps -aq -f status=exited -f name=$CONT_NAME)" ]; then
+        docker rm $CONT_NAME
+    fi
+    docker run -dit --name form-ular -p $PORT:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4
+fi
